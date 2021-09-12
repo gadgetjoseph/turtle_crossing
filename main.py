1a7c608f82661cb6d639c0a82fc972ad45f07427
Joe Mahaffey <joe@mahaffey.us>
2021-09-12 15:12:53 -0400

    Completed Code.

--- main.py
+++ main.py
@@ -6,9 +6,32 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("GadgetJoseph's Turle Crossing")
 screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.goto_start()
+        car_manager.level_up()
+        scoreboard.inrease_level()
+
+screen.exitonclick()
